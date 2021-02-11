ccd6082cc594db36e3a94672f5e8b15a0ff38ffc
aikanor <abalsubr@ucsd.edu>
2021-02-10 19:45:41 -0800

    integration 15/ (refactor encodetfbs_dataset)

--- wilds/datasets/encodetfbs_dataset.py
+++ wilds/datasets/encodetfbs_dataset.py
@@ -33,25 +33,43 @@ class EncodeTFBSDataset(WILDSDataset):
         self._y_size = 1
         self._n_classes = 2
         
-        # self._tr_chrs = ['chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr9', 'chr10', 'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chr20', 'chr22', 'chrX']
-        self._tr_chrs = ['chr2', 'chr9', 'chr11']
-        self._te_chrs = ['chr1', 'chr8', 'chr21']
+        # self._train_chroms = ['chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr9', 'chr10', 'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chr20', 'chr22', 'chrX']
+        self._train_chroms = ['chr2', 'chr9', 'chr11']
+        self._test_chroms = ['chr1', 'chr8', 'chr21']
         self._transcription_factor = 'MAX'
         self._train_celltypes = ['H1-hESC', 'HCT116', 'HeLa-S3', 'HepG2', 'K562']
         self._val_celltype = ['A549']
         self._test_celltype = ['GM12878']
+        self._all_chroms = self._train_chroms + self._test_chroms
         self._all_celltypes = self._train_celltypes + self._val_celltype + self._test_celltype
         
-        self._metadata_fields = ['chr', 'celltype', 'y']
         self._metadata_map = {}
-        self._metadata_map['chr'] = ['chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10', 'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chr20', 'chr21', 'chr22', 'chrX']
+        self._metadata_map['chr'] = self._all_chroms #['chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10', 'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chr20', 'chr21', 'chr22', 'chrX']
         self._metadata_map['celltype'] = self._all_celltypes
+        
+        # Get the splits
+        if split_scheme=='official':
+            split_scheme = 'standard'
+        
+        self._split_scheme = split_scheme
+        self._split_dict = {
+            'train': 0,
+            'id_val': 1,
+            'test': 2,
+            'val': 3
+        }
+        self._split_names = {
+            'train': 'Train',
+            'id_val': 'Validation (ID)',
+            'test': 'Test',
+            'val': 'Validation (OOD)',
+        }
         
         # Load sequence and DNase features
         sequence_filename = os.path.join(self._data_dir, 'sequence.npz')
         seq_arr = np.load(sequence_filename)
         self._seq_bp = {}
-        for chrom in seq_arr:
+        for chrom in self._all_chroms: #seq_arr:
             self._seq_bp[chrom] = seq_arr[chrom]
             print(chrom, time.time() - itime)
         
@@ -60,29 +78,27 @@ class EncodeTFBSDataset(WILDSDataset):
             dnase_filename = os.path.join(self._data_dir, '{}_dnase.npz'.format(ct))
             dnase_npz_contents = np.load(dnase_filename)
             self._dnase_allcelltypes[ct] = {}
-            for chrom in self._seq_bp:
+            for chrom in self._all_chroms: #self._seq_bp:
                 self._dnase_allcelltypes[ct][chrom] = dnase_npz_contents[chrom]
             print(ct, time.time() - itime)
         
         # Read in metadata dataframe from training+validation data
         train_regions_labeled = pd.read_csv(os.path.join(self._data_dir, 'labels/{}.train.labels.tsv.gz'.format(self._transcription_factor)), sep='\t')
         val_regions_labeled = pd.read_csv(os.path.join(self._data_dir, 'labels/{}.val.labels.tsv.gz'.format(self._transcription_factor)), sep='\t')
-        training_df = train_regions_labeled[np.isin(train_regions_labeled['chr'], self._tr_chrs)]
-        val_df = val_regions_labeled[np.isin(val_regions_labeled['chr'], self._te_chrs)]
+        training_df = train_regions_labeled[np.isin(train_regions_labeled['chr'], self._train_chroms)]
+        val_df = val_regions_labeled[np.isin(val_regions_labeled['chr'], self._test_chroms)]
         all_df = pd.concat([training_df, val_df])
-        print(train_regions_labeled, time.time() - itime)
         
-        # Filter by start/stop coordinate if needed 
-        # (TODO: remove for final version)
+        # Filter by start/stop coordinate if needed (TODO: remove for final version)
         filter_msk = all_df['start'] >= 0
         filter_msk = all_df['start']%1000 == 0
         all_df = all_df[filter_msk]
         
         pd_list = []
-        for ct in self._train_celltypes:
+        for ct in self._all_celltypes:
             tc_chr = all_df[['chr', 'start', 'stop', ct]]
             tc_chr.columns = ['chr', 'start', 'stop', 'y']
-            tc_chr['celltype'] = ct
+            tc_chr.insert(len(tc_chr.columns), 'celltype', ct)
             pd_list.append(tc_chr)
         metadata_df = pd.concat(pd_list)
         
@@ -91,33 +107,9 @@ class EncodeTFBSDataset(WILDSDataset):
         non_ambig_mask = (y_array != -1)
         metadata_df['y'] = y_array
         self._metadata_df = metadata_df[non_ambig_mask]
-        self._y_array = torch.LongTensor(y_array[non_ambig_mask])
         
-        chr_ints = self._metadata_df['chr'].replace(dict( [(y, x) for x, y in enumerate(self._metadata_map['chr'])] )).values
-        celltype_ints = self._metadata_df['celltype'].replace(dict( [(y, x) for x, y in enumerate(self._metadata_map['celltype'])] )).values
-        self._metadata_array = torch.stack(
-            (torch.LongTensor(chr_ints), 
-             torch.LongTensor(celltype_ints), 
-             self._y_array),
-            dim=1)
-        
-        # Get the splits
-        # TODO Extract splits as encoded in split_scheme. Hardcoded here for now.
-        self._split_scheme = split_scheme
-        self._split_dict = {
-            'train': 0,
-            'val-id': 1,
-            'test': 2,
-            'val-ood': 3
-        }
-        self._split_names = {
-            'train': 'Train',
-            'val-id': 'Validation (ID)',
-            'test': 'Test',
-            'val-ood': 'Validation (OOD)',
-        }
-        train_regions_mask = np.isin(self._metadata_df['chr'], self._tr_chrs)
-        val_regions_mask = np.isin(self._metadata_df['chr'], self._te_chrs)
+        train_regions_mask = np.isin(self._metadata_df['chr'], self._train_chroms)
+        val_regions_mask = np.isin(self._metadata_df['chr'], self._test_chroms)
         train_celltype_mask = np.isin(self._metadata_df['celltype'], self._train_celltypes)
         val_celltype_mask = np.isin(self._metadata_df['celltype'], self._val_celltype)
         test_celltype_mask = np.isin(self._metadata_df['celltype'], self._test_celltype)
@@ -125,17 +117,33 @@ class EncodeTFBSDataset(WILDSDataset):
         split_array = -1*np.ones(self._metadata_df.shape[0]).astype(int)
         split_array[np.logical_and(train_regions_mask, train_celltype_mask)] = self._split_dict['train']
         split_array[np.logical_and(val_regions_mask, test_celltype_mask)] = self._split_dict['test']
-        # Validate using test chr, either using a designated validation cell line ('val-ood') or a training cell line ('val-id')
-        split_array[np.logical_and(val_regions_mask, val_celltype_mask)] = self._split_dict['val-ood']
-        split_array[np.logical_and(val_regions_mask, train_celltype_mask)] = self._split_dict['val-id']
+        # Validate using test chr, either using a designated validation cell line ('val') or a training cell line ('id_val')
+        split_array[np.logical_and(val_regions_mask, val_celltype_mask)] = self._split_dict['val']
+        split_array[np.logical_and(val_regions_mask, train_celltype_mask)] = self._split_dict['id_val']
+        
         if self._split_scheme=='standard':
-            self._metadata_df['split'] = split_array
-            self._split_array = split_array
+            self._metadata_df.insert(len(self._metadata_df.columns), 'split', split_array)
         else:
             raise ValueError(f'Split scheme {self._split_scheme} not recognized')
+        
+        self._metadata_df = self._metadata_df[self._metadata_df['split'] != -1]
+        self._split_array = self._metadata_df['split'].values
+        
+        chr_ints = self._metadata_df['chr'].replace(dict( [(y, x) for x, y in enumerate(self._metadata_map['chr'])] )).values
+        celltype_ints = self._metadata_df['celltype'].replace(dict( [(y, x) for x, y in enumerate(self._metadata_map['celltype'])] )).values
+        self._y_array = torch.LongTensor(np.array(self._metadata_df['y']))
+        
+        self._metadata_array = torch.stack(
+            (torch.LongTensor(chr_ints), 
+             torch.LongTensor(celltype_ints), 
+             self._y_array),
+            dim=1)
+        self._metadata_fields = ['chr', 'celltype', 'y']
+        
         self._eval_grouper = CombinatorialGrouper(
             dataset=self,
             groupby_fields=['celltype'])
+        
         self._metric = Accuracy()
         
         super().__init__(root_dir, download, split_scheme)
@@ -152,8 +160,8 @@ class EncodeTFBSDataset(WILDSDataset):
         flank_size = 400
         interval_start = this_metadata['start'] - flank_size
         interval_end = this_metadata['stop'] + flank_size
-        dnase_this = _dnase_allcelltypes[this_metadata['celltype']][this_metadata['chr']][interval_start:interval_end]
-        seq_this = _seq_bp[this_metadata['chr']][interval_start:interval_end]
+        dnase_this = self._dnase_allcelltypes[this_metadata['celltype']][this_metadata['chr']][interval_start:interval_end]
+        seq_this = self._seq_bp[this_metadata['chr']][interval_start:interval_end]
         return torch.tensor(np.column_stack([seq_this, dnase_this]))
 
     def eval(self, y_pred, y_true, metadata):
