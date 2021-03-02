3ef4280dd80f106e7e9ac0245dc1ed7cf68928f2
Tony Lee <tonyh.lee@yahoo.com>
2021-03-01 19:35:32 -0800

    Fix subsampling Amazon

--- dataset_preprocessing/amazon_yelp/subsample_amazon.py
+++ dataset_preprocessing/amazon_yelp/subsample_amazon.py
@@ -16,14 +16,18 @@ Usage:
 """
 
 NOT_IN_DATASET = -1
+# Split: {'train': 0, 'val': 1, 'id_val': 2, 'test': 3, 'id_test': 4}
+TRAIN, OOD_VAL, ID_VAL, OOD_TEST, ID_TEST = range(5)
 
 
 def main(dataset_path, frac=0.25):
     def output_dataset_sizes(split_df):
         print("-" * 50)
-        print(f'Train size: {len(split_df[split_df["split"] == 0])}')
-        print(f'Val size: {len(split_df[split_df["split"] == 1])}')
-        print(f'Test size: {len(split_df[split_df["split"] == 2])}')
+        print(f'Train size: {len(split_df[split_df["split"] == TRAIN])}')
+        print(f'Val size: {len(split_df[split_df["split"] == OOD_VAL])}')
+        print(f'ID Val size: {len(split_df[split_df["split"] == ID_VAL])}')
+        print(f'Test size: {len(split_df[split_df["split"] == OOD_TEST])}')
+        print(f'ID Test size: {len(split_df[split_df["split"] == ID_TEST])}')
         print(
             f'Number of examples not included: {len(split_df[split_df["split"] == NOT_IN_DATASET])}'
         )
@@ -64,16 +68,73 @@ def main(dataset_path, frac=0.25):
     )
     print(subsampled_reviewers)
 
-    blackout_indices = []
-    for reviewer_id in subsampled_reviewers:
-        reviews = train_data_df[train_data_df["reviewerID"] == reviewer_id]
-        blackout_indices.extend(reviews.index)
+    blackout_indices = train_data_df[
+        train_data_df["reviewerID"].isin(subsampled_reviewers)
+    ].index
 
     # Mark all the corresponding reviews of blackout_indices as -1
     split_df.loc[blackout_indices, "split"] = NOT_IN_DATASET
     output_dataset_sizes(split_df)
 
+    # Mark duplicates
+    duplicated_within_user = data_df[["reviewerID", "reviewText"]].duplicated()
+    df_deduplicated_within_user = data_df[~duplicated_within_user]
+    duplicated_text = df_deduplicated_within_user[
+        df_deduplicated_within_user["reviewText"]
+        .apply(lambda x: x.lower())
+        .duplicated(keep=False)
+    ]["reviewText"]
+    duplicated_text = set(duplicated_text.values)
+    data_df["duplicate"] = (
+        data_df["reviewText"].isin(duplicated_text)
+    ) | duplicated_within_user
+
+    # Mark html candidates
+    data_df["contains_html"] = data_df["reviewText"].apply(
+        lambda x: "<" in x and ">" in x
+    )
+
+    # Mark clean ones
+    data_df["clean"] = ~data_df["duplicate"] & ~data_df["contains_html"]
+
+    # Clear ID val and ID test since we're regenerating
+    split_df.loc[split_df["split"] == ID_VAL, "split"] = NOT_IN_DATASET
+    split_df.loc[split_df["split"] == ID_TEST, "split"] = NOT_IN_DATASET
+
+    # Regenerate ID val and ID test
+    train_reviewer_ids = data_df[split_df["split"] == TRAIN]["reviewerID"].unique()
+    cutoff = int(len(train_reviewer_ids) / 2)
+    id_val_reviewer_ids = train_reviewer_ids[:cutoff]
+    id_test_reviewer_ids = train_reviewer_ids[cutoff:]
+    split_df.loc[
+        (split_df["split"] == NOT_IN_DATASET)
+        & data_df["clean"]
+        & data_df["reviewerID"].isin(id_val_reviewer_ids),
+        "split",
+    ] = ID_VAL
+    split_df.loc[
+        (split_df["split"] == NOT_IN_DATASET)
+        & data_df["clean"]
+        & data_df["reviewerID"].isin(id_test_reviewer_ids),
+        "split",
+    ] = ID_TEST
+
+    # Sanity check
+    assert (
+        data_df[(split_df["split"] == ID_VAL)]["reviewerID"].value_counts().min() == 75
+    )
+    assert (
+        data_df[(split_df["split"] == ID_VAL)]["reviewerID"].value_counts().max() == 75
+    )
+    assert (
+        data_df[(split_df["split"] == ID_TEST)]["reviewerID"].value_counts().min() == 75
+    )
+    assert (
+        data_df[(split_df["split"] == ID_TEST)]["reviewerID"].value_counts().max() == 75
+    )
+
     # Write out the new splits to user.csv
+    output_dataset_sizes(split_df)
     split_df.to_csv(user_csv_path, index=False)
     print("Done.")
 
