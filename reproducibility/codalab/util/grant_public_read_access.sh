4abba8380e17f2ca4d3f04fce31704f87d4b4040
Tony Lee <tonyh.lee@yahoo.com>
2021-03-05 22:15:13 -0800

    Remove old file

--- reproducibility/codalab/util/grant_public_read_access.sh
+++ reproducibility/codalab/util/grant_public_read_access.sh
@@ -1,9 +1,9 @@
 #
-# Run this script to give the public read access to Wilds CodaLab worksheet and bundles.
+# Run this script to give the public read access to Wilds CodaLab worksheets and bundles.
 #
 # Usage: bash reproducibility/codalab/util/grant_public_read_access.sh
 #
-for i in $(cl wsearch wilds-results .limit=5 -u); do
+for i in $(cl wsearch wilds-results .limit=10 -u); do
     cl wperm ${i} public read || true;
     cl perm $(cl search .mine host_worksheet=${i} .limit=1000 -u) public read || true;
 done
