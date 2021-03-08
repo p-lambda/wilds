60acde17c1becc18c3f98866c622bb80bdae9f67
Tony Lee <tonyh.lee@yahoo.com>
2021-03-07 16:04:37 -0800

    Updated coral penalty weight for iWildCam

--- examples/configs/datasets.py
+++ examples/configs/datasets.py
@@ -153,7 +153,7 @@ dataset_defaults = {
         'groupby_fields': ['location',],
         'n_groups_per_batch': 2,
         'irm_lambda': 1.,
-        'coral_penalty_weight': 1.,
+        'coral_penalty_weight': 10.,
         'no_group_logging': True,
         'process_outputs_function': 'multiclass_logits_to_pred'
     },
