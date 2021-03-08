58956af7514219bcd9c74a00a49fbf4143a8745d
Pang Wei Koh <pangwei@cs.stanford.edu>
2021-03-08 13:31:30 -0800

    eval epoch comments

--- examples/run_expt.py
+++ examples/run_expt.py
@@ -93,7 +93,7 @@ def main():
     parser.add_argument('--evaluate_all_splits', type=parse_bool, const=True, nargs='?', default=True)
     parser.add_argument('--eval_splits', nargs='+', default=[])
     parser.add_argument('--eval_only', type=parse_bool, const=True, nargs='?', default=False)
-    parser.add_argument('--eval_epoch', default=None, type=int)
+    parser.add_argument('--eval_epoch', default=None, type=int, help='If eval_only is set, then eval_epoch allows you to specify evaluating at a particular epoch. By default, it evaluates the best epoch by validation performance.')
 
     # Misc
     parser.add_argument('--device', type=int, default=0)
