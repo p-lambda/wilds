70ccb105d28b3a2f09d040a4c3e70681313b9a00
Pang Wei Koh <pangwei@cs.stanford.edu>
2021-03-08 12:41:37 -0800

    clean up supported_datasets

--- wilds/__init__.py
+++ wilds/__init__.py
@@ -1,22 +1,6 @@
 from .version import __version__
 from .get_dataset import get_dataset
 
-supported_datasets = [
-    'amazon',
-    'camelyon17',
-    'celebA',
-    'civilcomments',
-    'iwildcam',
-    'waterbirds',
-    'yelp',
-    'ogb-molpcba',
-    'poverty',
-    'fmow',
-    'bdd100k',
-    'py150',
-    'sqf',
-]
-
 benchmark_datasets = [
     'amazon',
     'camelyon17',
@@ -27,3 +11,13 @@ benchmark_datasets = [
     'fmow',
     'py150',
 ]
+
+additional_datasets = [
+    'celebA',
+    'waterbirds',
+    'yelp',
+    'bdd100k',
+    'sqf',
+]
+
+supported_datasets = benchmark_datasets + additional_datasets
