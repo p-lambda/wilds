fix DNase normalization

dataset_preprocessing/encode-tfbs/prep_accessibility.py
@@ -10,28 +10,32 @@ chrom_sizes = {'chr1': 249250621, 'chr10': 135534747, 'chr11': 135006516, 'chr12
 
 def qn_sample_to_array(
     input_celltypes, 
+    input_chroms=None, 
     subsampling_ratio=1000, 
     data_pfx = '/users/abalsubr/wilds/examples/data/encode-tfbs_v1.0/'
 ):
     itime = time.time()
+    if input_chroms is None:
+        input_chroms = chrom_sizes.keys()
+    qn_chrom_sizes = { k: chrom_sizes[k] for k in input_chroms }
     # chromosome-specific subsampling seeds
     chr_to_seed = {}
     i = 0
-    for the_chr in chrom_sizes:
+    for the_chr in qn_chrom_sizes:
         chr_to_seed[the_chr] = i
         i += 1
 
     # subsampling; multiple replicates are added
-    sample_len = np.ceil(np.array(list(chrom_sizes.values()))/subsampling_ratio).astype(int)
+    sample_len = np.ceil(np.array(list(qn_chrom_sizes.values()))/subsampling_ratio).astype(int)
     sample = np.zeros(sum(sample_len))
     start = 0
     j = 0
-    for the_chr in chrom_sizes:
+    for the_chr in qn_chrom_sizes:
         np.random.seed(chr_to_seed[the_chr])
         for ct in input_celltypes:
             path = data_pfx + 'DNASE.{}.fc.signal.bigwig'.format(ct)
             bw = pyBigWig.open(path)
-            signal = np.nan_to_num(np.array(bw.values(the_chr, 0, chrom_sizes[the_chr])))
+            signal = np.nan_to_num(np.array(bw.values(the_chr, 0, qn_chrom_sizes[the_chr])))
             index = np.random.randint(0, len(signal), sample_len[j])
             sample[start:(start+sample_len[j])] += (1.0/len(input_celltypes))*signal[index]
         start += sample_len[j]
@@ -104,6 +108,7 @@ def dnase_normalize(
     input_bw_celltype, 
     sample_celltype, 
     ref_celltypes, 
+    out_fname = 'norm', 
     data_pfx = '/users/abalsubr/wilds/examples/data/encode-tfbs_v1.0/'
 ):
     itime = time.time()
@@ -113,7 +118,8 @@ def dnase_normalize(
         ref += (1.0/len(ref_celltypes))*np.load(data_pfx + "qn.{}.npy".format(ct))
 
     chromsizes_list = [(k, v) for k, v in chrom_sizes.items()]
-    bw_output = pyBigWig.open(data_pfx + 'DNase.{}.norm.bigwig'.format(input_bw_celltype), 'w')
+    bw_output = pyBigWig.open(data_pfx + 'DNase.{}.{}.bigwig'.format(
+        input_bw_celltype, out_fname), 'w')
     bw_output.addHeader(chromsizes_list)
     # bw_output.addHeader(list(zip(chr_all , num_bp)), maxZooms=0) # zip two turples
     
@@ -126,7 +132,7 @@ def dnase_normalize(
         # write normalized dnase file.
         chroms = np.array([the_chr] * len(vals_anchored))
         bw_output.addEntries(chroms, starts, ends=ends, values=vals_anchored)
-        print(the_chr, time.time() - itime)
+        print(input_bw_celltype, the_chr, time.time() - itime)
 
     bw_output.close()
 
@@ -156,18 +162,18 @@ def generate_accessibility_archives(input_dir='dnase_bigwigs', output_dir='codal
 
 
 if __name__ == '__main__':
+    train_chroms = ['chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr10', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chr20', 'chr22', 'chrX']
     ch_train_celltypes = ['H1-hESC', 'HCT116', 'HeLa-S3', 'K562', 'A549', 'GM12878']
     ch_val_celltype = ['HepG2']
     ch_test_celltype = ['liver']
-    ref_celltypes = ch_train_celltypes + ch_val_celltype
-    all_celltypes = ref_celltypes + ch_test_celltype
+    ref_celltypes = ch_train_celltypes
+    all_celltypes = ch_train_celltypes + ch_val_celltype + ch_test_celltype
     for ct in all_celltypes:
-        qn_sample_to_array([ct])
+        qn_sample_to_array([ct], input_chroms=train_chroms)
+    
+    # Create normalized bigwigs for OOD validation split.
     for ct in all_celltypes:
         dnase_normalize(ct, ct, ref_celltypes)
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument('--input_dir', required=True)
-#     parser.add_argument('--output_dir', required=True)
-#     args = parser.parse_args()
-
-    # generate_accessibility_archives(input_dir=args.input_dir, output_dir=args.output_dir)
+    # Create normalized bigwig for ID validation split.
+    for ct in ch_test_celltype:
+        dnase_normalize(ct, ct, ch_test_celltype)