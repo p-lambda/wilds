c0574e1ba53478d4804a376e8ab7c22a71962387
Tony Lee <tonyh.lee@yahoo.com>
2021-04-15 20:32:38 -0700

    Update variable name for example

--- examples/other/load_unlabeled_amazon.py
+++ examples/other/load_unlabeled_amazon.py
@@ -9,8 +9,8 @@ from wilds import get_dataset
 from wilds.common.data_loaders import get_train_loader
 
 dataset = get_dataset(dataset="amazon", unlabeled=True, download=True)
-unlabeled_data = dataset.get_subset("extra_unlabeled")
-data_loader = get_train_loader("standard", unlabeled_data, batch_size=16)
+unlabeled_subset = dataset.get_subset("extra_unlabeled")
+data_loader = get_train_loader("standard", unlabeled_subset, batch_size=16)
 
 for x, metadata in data_loader:
     pdb.set_trace()
