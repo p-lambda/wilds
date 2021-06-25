6e52e976d9115114c8e722cca68b6b1753b8d20d
i-gao <igao@stanford.edu>
2021-06-24 23:17:19 -0700

    efficientnet models

--- examples/models/initializer.py
+++ examples/models/initializer.py
@@ -7,6 +7,7 @@ from models.layers import Identity
 from models.gnn import GINVirtual
 from models.code_gpt import GPT2LMHeadLogit, GPT2FeaturizerLMHeadLogit
 from transformers import GPT2Tokenizer
+from efficientnet_pytorch import EfficientNet
 
 def initialize_model(config, d_out, is_featurizer=False):
     """
@@ -36,6 +37,13 @@ def initialize_model(config, d_out, is_featurizer=False):
                 name=config.model,
                 d_out=d_out,
                 **config.model_kwargs)
+    elif 'efficientnet' in config.model:
+        if is_featurizer:
+            featurizer = initialize_efficientnet_model(config, d_out, is_featurizer)
+            classifier = nn.Linear(featurizer.d_out, d_out)
+            model = (featurizer, classifier)
+        else:
+            model = initialize_efficientnet_model(config, d_out)
     elif 'bert' in config.model:
         if is_featurizer:
             featurizer = initialize_bert_based_model(config, d_out, is_featurizer)
@@ -122,4 +130,13 @@ def initialize_torchvision_model(name, d_out, **kwargs):
         last_layer = nn.Linear(d_features, d_out)
         model.d_out = d_out
     setattr(model, last_layer_name, last_layer)
+    return model
+
+def initialize_efficientnet_model(config, d_out, is_featurizer=False):
+    model = EfficientNet.from_pretrained(config.model, num_classes=d_out)
+    if is_featurizer: # want a featurizer, so set _fc layer to identity
+        d_features = getattr(model, '_fc').in_features
+        last_layer = Identity(d_features)
+        model.d_out = d_features
+        setattr(model, '_fc', last_layer)
     return model
