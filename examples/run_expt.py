b0831742e56fba6522d6a0e2a38bf3c4f11cfc3c
i-gao <igao@stanford.edu>
2021-07-22 10:48:34 -0700

    fixes to make runnable

--- examples/run_expt.py
+++ examples/run_expt.py
@@ -144,8 +144,11 @@ def main():
             raise ValueError(f"Specified {len(config.device)} devices, but only {device_count} devices found.")
 
         config.use_data_parallel = len(config.device) > 1
-        device_str = ",".join(map(str, config.device))
-        config.device = torch.device(f"cuda:{device_str}")
+        try: 
+            device_str = ",".join(map(str, config.device))
+            config.device = torch.device(f"cuda:{device_str}")
+        except:
+            config.device = torch.device("cuda")
     else:
         config.use_data_parallel = False
         config.device = torch.device("cpu")
@@ -217,8 +220,6 @@ def main():
                 config.train_transform, config, full_unlabeled_dataset, additional_transform_name="fixmatch"
             )
         elif config.algorithm == "noisy_student":
-            import pdb 
-            pdb.set_trace()
             # For FixMatch, we need our loader to return batches in the form ((x_weak, x_strong), m)
             # We do this by initializing a special transform function
             unlabeled_train_transform = initialize_transform(
@@ -228,8 +229,6 @@ def main():
             unlabeled_train_transform = train_transform
         
         if config.algorithm == "noisy_student": 
-            import pdb 
-            pdb.set_trace()
             # For Noisy Student, we need to first generate pseudolabels using the teacher
             # and then prep the unlabeled dataset to return these pseudolabels in __getitem__
             assert config.teacher_model_path is not None
