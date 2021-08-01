60676d75c8ff81763d4d707bd0222e75bb057474
i-gao <igao@stanford.edu>
2021-07-31 20:25:15 -0700

    remove some dev print-outs for loading models

--- examples/utils.py
+++ examples/utils.py
@@ -99,12 +99,11 @@ def load(module, path, device=None, tries=2):
         for _ in range(tries):
             state = match_keys(state, list(module_keys))
             module.load_state_dict(state, strict=False)
-            print(f"parameters in module but not in state: {module_keys-state.keys()}") # TODO: remove, testing code
             leftover_state = {k:v for k,v in state.items() if k in list(state.keys()-module_keys)}
             leftover_module_keys = module_keys - state.keys()
             if len(leftover_state) == 0 or len(leftover_module_keys) == 0: break
             state, module_keys = leftover_state, leftover_module_keys
-        print(f"Some module parameters could not be found in the loaded state: {module_keys-state.keys()}")
+        if len(module_keys-state.keys()) > 0: print(f"Some module parameters could not be found in the loaded state: {module_keys-state.keys()}")
     return prev_epoch, best_val_metric
 
 def match_keys(d, ref):
