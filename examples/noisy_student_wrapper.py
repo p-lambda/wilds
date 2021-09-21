ba8a11e89c9f082150620744c38a9ac777d83f8f
i-gao <igao@stanford.edu>
2021-09-20 21:13:41 -0700

    change ns wrapper to warm start students and take in an init teacher model

--- examples/noisy_student_wrapper.py
+++ examples/noisy_student_wrapper.py
@@ -1,18 +1,18 @@
 """
-Helper code to run multiple iterations of Noisy Student, using the same hyperparameters between iterations.
+Helper code to run multiple iterations of Noisy Student, using the same hyperparameters between iterations. The initial teacher's weights must be provided by the command line.
 
-Normally, to run 2 iterations, one would run a sequence of commands:
-    python examples/run_expt.py --root_dir $HOME --log_dir ./teacher --dataset DATASET --algorithm NoisyStudent --unlabeled_split test_unlabeled
-    python examples/run_expt.py --root_dir $HOME --log_dir ./student1 --dataset DATASET --algorithm NoisyStudent --unlabeled_split test_unlabeled --teacher_model_path ./teacher/model.pth
-    python examples/run_expt.py --root_dir $HOME --log_dir ./student2 --dataset DATASET --algorithm NoisyStudent --unlabeled_split test_unlabeled --teacher_model_path ./student1/model.pth
+Normally, to run 2 warm-started iterations with some initial teacher weights, one would run a sequence of commands:
+    python examples/run_expt.py --root_dir $HOME --log_dir ./student1 --dataset DATASET --algorithm NoisyStudent --unlabeled_split test_unlabeled --teacher_model_path teacher_weights.pth --pretrained_model_path teacher_weights.pth
+    python examples/run_expt.py --root_dir $HOME --log_dir ./student2 --dataset DATASET --algorithm NoisyStudent --unlabeled_split test_unlabeled --teacher_model_path ./student1/model.pth --pretrained_model_path ./student1/model.pth
 
-With this script, to run 2 iterations:
-    python examples/noisy_student_wrapper.py 2 --root_dir $HOME --log_dir . --dataset DATASET --unlabeled_split test_unlabeled
+With this script, to run 2 warm-started iterations with some initial teacher weights:
+    python examples/noisy_student_wrapper.py 2 teacher_weights.pth --root_dir $HOME --log_dir . --dataset DATASET --unlabeled_split test_unlabeled
 
 i.e. usage:
-    python examples/noisy_student_wrapper.py [NUM_ITERS] [REST OF RUN_EXPT COMMAND STRING]
+    python examples/noisy_student_wrapper.py [NUM_ITERS] [INITIAL_TEACHER_WEIGHTS] [REST OF RUN_EXPT COMMAND STRING]
 
 Notes:
+    - Students are all warm-started with the current teacher's weights.
     - This command will use the FIRST occurrence of --log_dir (instead of the last).
 """
 import argparse
@@ -22,9 +22,11 @@ import subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument("num_iters", type=int)
+parser.add_argument("initial_teacher_path", type=str) # required
 parser.add_argument("cmd", nargs=argparse.REMAINDER)
 args = parser.parse_args()
 
+assert args.initial_teacher_path.endswith('.pth')
 prefix = pathlib.Path(__file__).parent.resolve()
 
 def remove_arg(args, arg_to_remove):
@@ -59,20 +61,18 @@ unlabeled_split = remove_arg(args, "unlabeled_split")
 gradient_accumulation_steps = remove_arg(args, "gradient_accumulation_steps")
 n_epochs = remove_arg(args, "n_epochs")
 
-# Run teacher
-cmd = f"python {prefix}/run_expt.py --algorithm NoisyStudent {' '.join(args.cmd)} --log_dir {log_dir}/teacher"
-print(f">>> Running {cmd}")
-subprocess.Popen(cmd, shell=True).wait()
-
 # Run student iters
 for i in range(1, args.num_iters + 1):
+    if (i == 1):
+        teacher_weights = args.initial_teacher_path
+    else:
+        teacher_weights = f"{log_dir}/student{i-1}/{dataset}_seed:{seed}_epoch:best_model.pth"
     cmd = (
         f"python {prefix}/run_expt.py --algorithm NoisyStudent {' '.join(args.cmd)}"
-        f" --unlabeled_split {unlabeled_split} --gradient_accumulation_steps {gradient_accumulation_steps}"
-        f" --n_epochs {n_epochs} --log_dir {log_dir}/student{i}"
-        + f" --teacher_model_path {log_dir}/"
-        + ("teacher" if i == 1 else f"student{i-1}")
-        + f"/{dataset}_seed:{seed}_epoch:best_model.pth"
+        + f" --unlabeled_split {unlabeled_split} --gradient_accumulation_steps {gradient_accumulation_steps}"
+        + f" --n_epochs {n_epochs} --log_dir {log_dir}/student{i}"
+        + f" --teacher_model_path {teacher_weights}"
+        + f" --pretrained_model_path {teacher_weights}" # warm starting        
     )
     print(f">>> Running {cmd}")
     subprocess.Popen(cmd, shell=True).wait()
