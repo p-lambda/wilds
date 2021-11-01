ca20b80af6ebeede575ee95ddac8bf7260c746c5
i-gao <igao@stanford.edu>
2021-11-01 09:37:00 -0700

    probe labels in WildUnlabeledDataset

--- examples/run_expt.py
+++ examples/run_expt.py
@@ -240,6 +240,8 @@ def main():
             unlabeled=True,
             **config.dataset_kwargs
         )
+        import pdb
+        pdb.set_trace()
         train_grouper = CombinatorialGrouper(
             dataset=[full_dataset, full_unlabeled_dataset],
             groupby_fields=config.groupby_fields
