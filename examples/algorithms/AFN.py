43de08a2e76b7d4f96ac64953fe01ae8da6e7979
Tony Lee <tonyh.lee@yahoo.com>
2021-11-09 17:13:48 -0800

    AFN model

--- examples/algorithms/AFN.py
+++ examples/algorithms/AFN.py
@@ -1,25 +1,31 @@
-import torch
+import math
+from typing import Optional, List, Dict
 
-from algorithms.single_model_algorithm import SingleModelAlgorithm
-from models.initializer import initialize_model
+import torch
+import torch.nn as nn
 
 try:
     from torch_geometric.data import Batch
 except ImportError:
     pass
 
+from algorithms.single_model_algorithm import SingleModelAlgorithm
+from models.initializer import initialize_model
+
 
 class AFN(SingleModelAlgorithm):
     """
     Adaptive Feature Norm (AFN)
 
-    @InProceedings{Xu_2019_ICCV,
-        author = {Xu, Ruijia and Li, Guanbin and Yang, Jihan and Lin, Liang},
-        title = {Larger Norm More Transferable: An Adaptive Feature Norm Approach for Unsupervised Domain Adaptation},
-        booktitle = {The IEEE International Conference on Computer Vision (ICCV)},
-        month = {October},
-        year = {2019}
-    }
+    Original paper:
+        @InProceedings{Xu_2019_ICCV,
+            author = {Xu, Ruijia and Li, Guanbin and Yang, Jihan and Lin, Liang},
+            title = {Larger Norm More Transferable: An Adaptive Feature Norm Approach for
+                     Unsupervised Domain Adaptation},
+            booktitle = {The IEEE International Conference on Computer Vision (ICCV)},
+            month = {October},
+            year = {2019}
+        }
     """
 
     def __init__(
@@ -32,10 +38,10 @@ class AFN(SingleModelAlgorithm):
         n_train_steps,
     ):
         # Initialize model
-        featurizer, classifier = initialize_model(
+        featurizer, _ = initialize_model(
             config, d_out=d_out, is_featurizer=True
         )
-        model = torch.nn.Sequential(featurizer, classifier)
+        model = AFNModel(featurizer, d_out=d_out)
 
         # Initialize module
         super().__init__(
@@ -46,11 +52,6 @@ class AFN(SingleModelAlgorithm):
             metric=metric,
             n_train_steps=n_train_steps,
         )
-
-        # Model components
-        self.featurizer = featurizer
-        self.classifier = classifier
-
         # Algorithm hyperparameters
         self.penalty_weight = config.afn_penalty_weight
 
@@ -58,7 +59,7 @@ class AFN(SingleModelAlgorithm):
         self.logged_fields.append("classification_loss")
         self.logged_fields.append("afn_loss")
 
-    def afn_loss_soft(self, features):
+    def afn_loss_stepwise(self, features):
         """
         Adapted from https://github.com/jihanyang/AFN
         """
@@ -100,8 +101,7 @@ class AFN(SingleModelAlgorithm):
         x_cat = x_cat.to(self.device)
         y_true = y_true.to(self.device)
 
-        features = self.featurizer(x_cat)
-        y_pred = self.classifier(features)
+        y_pred, features = self.model(x_cat)
         # Ignore the predicted labels for the unlabeled data
         y_pred = y_pred[: len(y_true)]
 
@@ -120,11 +120,13 @@ class AFN(SingleModelAlgorithm):
 
         if self.is_training:
             features = results.pop("features")
-            features_source = features[: len(results["y_true"])]
-            features_target = features[len(results["y_true"]):]
-            afn_loss = self.afn_loss(features_source) + self.afn_loss(features_target)
+            f_source = features[: len(results["y_true"])]
+            f_target = features[len(results["y_true"]) :]
+            afn_loss = self.afn_loss(f_source) + self.afn_loss(f_target)
         else:
             afn_loss = 0.0
+
+        import pdb; pdb.set_trace()
 
         # Add to results for additional logging
         self.save_metric_for_logging(
@@ -132,3 +134,63 @@ class AFN(SingleModelAlgorithm):
         )
         self.save_metric_for_logging(results, "afn_loss", afn_loss)
         return classification_loss + self.penalty_weight * afn_loss
+
+
+class AFNModel(nn.Module):
+    def __init__(self, featurizer, d_out, bottleneck_dim: Optional[int] = 1000):
+        super().__init__()
+        self.featurizer = featurizer
+        self.bottleneck = nn.Sequential(Block(featurizer.d_out, bottleneck_dim=bottleneck_dim))
+        self.classifier = nn.Linear(bottleneck_dim, d_out)
+
+    def forward(self, x):
+        features = self.featurizer(x)
+        features = self.bottleneck(features)
+        predictions = self.classifier(features)
+        return predictions, features
+
+
+class Block(nn.Module):
+    r"""
+    Adapted from https://github.com/thuml/Transfer-Learning-Library.
+
+    Basic building block for Image Classifier with structure: FC-BN-ReLU-Dropout.
+    We use :math:`L_2` preserved dropout layers.
+    Given mask probability :math:`p`, input :math:`x_k`, generated mask :math:`a_k`,
+    vanilla dropout layers calculate
+
+    .. math::
+        \hat{x}_k = a_k\frac{1}{1-p}x_k\\
+
+    While in :math:`L_2` preserved dropout layers
+
+    .. math::
+        \hat{x}_k = a_k\frac{1}{\sqrt{1-p}}x_k\\
+
+    Args:
+        in_features (int): Dimension of input features
+        bottleneck_dim (int, optional): Feature dimension of the bottleneck layer. Default: 1000
+        dropout_p (float, optional): dropout probability. Default: 0.5
+    """
+
+    def __init__(
+        self,
+        in_features: int,
+        bottleneck_dim: Optional[int] = 1000,
+        dropout_p: Optional[float] = 0.5,
+    ):
+        super(Block, self).__init__()
+        self.fc = nn.Linear(in_features, bottleneck_dim)
+        self.bn = nn.BatchNorm1d(bottleneck_dim, affine=True)
+        self.relu = nn.ReLU(inplace=True)
+        self.dropout = nn.Dropout(dropout_p)
+        self.dropout_p = dropout_p
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        f = self.fc(x)
+        f = self.bn(f)
+        f = self.relu(f)
+        f = self.dropout(f)
+        if self.training:
+            f.mul_(math.sqrt(1 - self.dropout_p))
+        return f
