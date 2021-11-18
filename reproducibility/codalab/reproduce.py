2000431f6aa04ccc337d73a3e47b4e9a10d71575
Tony Lee <tonyh.lee@yahoo.com>
2021-11-18 13:48:21 -0800

    cleanup

--- reproducibility/codalab/reproduce.py
+++ reproducibility/codalab/reproduce.py
@@ -256,7 +256,7 @@ class CodaLabReproducibility:
 
                 experiment_name = (
                     f"{dataset}_{algorithm.lower()}{'_coarse' if coarse else ''}{'_weak' if weak else ''}"
-                    f"{'_hafn' if use_hafn else ''}_adjustsamelrgridstrongaug"
+                    f"{'_hafn' if use_hafn else ''}"
                 )
                 if unlabeled_split:
                     experiment_name += f"_{unlabeled_split.replace('_', '')}"
