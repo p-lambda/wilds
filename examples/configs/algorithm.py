6bcfd0a5116f03aff776b95d3020ed30a273c0ae
i-gao <igao@stanford.edu>
2021-12-10 17:06:36 -0600

    clean up algorithm configs, add todos for filling in default hparams

--- examples/configs/algorithm.py
+++ examples/configs/algorithm.py
@@ -36,6 +36,10 @@ algorithm_defaults = {
         'eval_loader': 'standard',
         'randaugment_n': 2,
         'additional_train_transform': 'randaugment',     # Apply strong augmentation to labeled & unlabeled examples
+        # 'dann_penalty_weight': 0, # TODO: fill in
+        # 'dann_classifier_lr': 0, # TODO: fill in
+        # 'dann_featurizer_lr': 0, # TODO: fill in
+        # 'dann_discriminator_lr': 0, # TODO: fill in
     },
     'AFN': {
         'train_loader': 'standard',
@@ -47,13 +51,17 @@ algorithm_defaults = {
         'hafn_r': 1.0,
         'additional_train_transform': 'randaugment',    # Apply strong augmentation to labeled & unlabeled examples
         'randaugment_n': 2,
+        # 'afn_penalty_weight': 0, #TODO: fill in
+        # 'safn_delta_r': 0, #TODO: fill in
+        # 'hafn_r': 0, #TODO: fill in
+        # 'use_hafn': False, #TODO: fill in
     },
     'FixMatch': {
         'train_loader': 'standard',
         'uniform_over_groups': False,
         'eval_loader': 'standard',
-        'self_training_lambda': 1,
-        'self_training_threshold': 0.7,
+        'self_training_lambda': 1, # TODO: fill in
+        'self_training_threshold': 0.7, # TODO: fill in
         'scheduler': 'FixMatchLR',
         'randaugment_n': 2,
         'additional_train_transform': 'randaugment',     # Apply strong augmentation to labeled examples
@@ -62,9 +70,9 @@ algorithm_defaults = {
         'train_loader': 'standard',
         'uniform_over_groups': False,
         'eval_loader': 'standard',
-        'self_training_lambda': 1,
-        'self_training_threshold': 0.7,
-        'pseudolabel_T2': 0.4,
+        'self_training_lambda': 1, # TODO: fill in
+        'self_training_threshold': 0.7, # TODO: fill in
+        'pseudolabel_T2': 0.4, # TODO: fill in
         'scheduler': 'FixMatchLR',
         'randaugment_n': 2,
         'additional_train_transform': 'randaugment',     # Apply strong augmentation to labeled & unlabeled examples
@@ -75,6 +83,7 @@ algorithm_defaults = {
         'eval_loader': 'standard',
         'noisystudent_add_dropout': True,
         'noisystudent_dropout_rate': 0.5,
+        'noisystudent_soft_pseudolabels': False,
         'scheduler': 'FixMatchLR',
         'randaugment_n': 2,
         'additional_train_transform': 'randaugment',     # Apply strong augmentation to labeled & unlabeled examples
