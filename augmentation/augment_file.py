3993e0ca57a25894dab1d1c61ee9c1491121de96
Luka Jovanovic <luca.jovanovich@gmail.com>
2023-11-18 15:00:49 -0500

    Handle the duplication of metadata

--- augmentation/augment_file.py
+++ augmentation/augment_file.py
@@ -42,7 +42,46 @@ def process_file(input_file_path, output_file_path, combined_file_path, k=1):
         with open(output_file_path, 'r') as new_file:
             combined_file.write(new_file.read())
 
-input_file = 'data/py150_v1.0/raw/train.txt'
-output_file = 'data-aug/py150_v1.0/raw/train-aug.txt'
-combined_file = 'data-aug/py150_v1.0/raw/train.txt'
+def duplicate_meta_content(meta_file_path, output_meta_path):
+    # Ensure the output directory exists
+    os.makedirs(os.path.dirname(output_meta_path), exist_ok=True)
+
+    with open(meta_file_path, 'r') as meta_file:
+        meta_lines = meta_file.read().splitlines()
+
+    # Duplicate the contents
+    duplicated_content = meta_lines + meta_lines
+
+    # Write the duplicated content to the output file
+    with open(output_meta_path, 'w') as output_meta:
+        output_meta.write('\n'.join(duplicated_content))
+
+# ATTENTION!!! BE EXTREMELY CAREFUL THAT THE PATHS ARE RIGHT WHEN RUNNING THIS SCRIPT
+# Below are the paths for the full set and the 500 set. If you make a new set,
+# you will need to add the paths. Otherwise, just comment out the ones you don't need.
+
+######################## Augment snippet files ###############################
+
+# Augment the full set `data/py150_v1.0/raw/train.txt`
+# input_file = 'data/py150_v1.0/raw/train.txt'
+# output_file = 'data-aug/py150_v1.0/raw/train-aug.txt'
+# combined_file = 'data-aug/py150_v1.0/raw/train.txt'
+
+# Augment the 500 set `data500/py150_v1.0/raw/train.txt`
+input_file = 'data500/py150_v1.0/raw/train.txt'
+output_file = 'data500-aug/py150_v1.0/raw/train-aug.txt'
+combined_file = 'data500-aug/py150_v1.0/raw/train.txt'
+
 process_file(input_file, output_file, combined_file)
+
+######################## Duplicate meta files ###############################
+
+# Duplicate the full set metadata `data/py150_v1.0/metadata/repo_file_names/train.txt`
+# meta_file_path = 'data/py150_v1.0/metadata/repo_file_names/train.txt'
+# output_meta_path = 'data-aug/py150_v1.0/metadata/repo_file_names/train.txt'
+
+# Duplicate the 500 set metadata `data500/py150_v1.0/metadata/repo_file_names/train.txt`
+meta_file_path = 'data500/py150_v1.0/metadata/repo_file_names/train.txt'
+output_meta_path = 'data500-aug/py150_v1.0/metadata/repo_file_names/train.txt'
+
+duplicate_meta_content(meta_file_path, output_meta_path)
